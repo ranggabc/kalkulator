ebfb1e31a618e617dfa12c685dbd4c36d1d6895a
ranggabc <ranggabayu778@gmail.com>
2022-09-15 22:53:11 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Aplikasi Kalkulator
+
+Aplikasi kalkulator sederhana ini dibuat untuk pembelajaran saya dalam menggunakan bahasa pemrograman Java.
+
+- Penjumlahan
+- Pengurangan
+- Perkalian
+- Pembagian
+
+## Fitur
+
+- Jika hasil adalah bilangan genap maka akan tampil pemberitahuan "Nilai Bilangan Genap".
+- Jika hasil adalah bilangan ganjil maka akan tampil pemberitahuan "Nilai Bilangan Ganjil".
+- Jika hasil adalah "0" maka akan tampil pemberitahuan "Nilai adalah NOL".
---
+++ src/calculator/Joption.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Rangga Bayu
+ */
+class Joption {
+
+    static void showMessageDialog(JButton btnhasil, String hasil_Genap) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
